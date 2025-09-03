388fe163884b22553a7ef151be60d73fbf90920f
Tobiloba Ogundiyan <apprentice_py@icloud.com>
2025-09-03 06:35:02 -0400

    added routing configuration

--- infra/main.tf
+++ infra/main.tf
@@ -55,4 +55,37 @@ resource "aws_nat_gateway" "nat" {
   }
 }
 
+### Routing
+resource "aws_route_table" "public" {
+  vpc_id =  aws_vpc.main.id
+  route {
+    # route all web traffic to internet gateway, since nat is in this subnet it gets internet access too
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.9/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+# associating subnets with route tables
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.private.id
+}
+
+
 
