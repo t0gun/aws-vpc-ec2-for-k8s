de8df5d4e302a89d639a0ee5a57339a97aff9391
Tobiloba Ogundiyan <apprentice_py@icloud.com>
2025-09-06 09:31:47 -0400

    added seperate variables folder

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,29 @@
 variable "region" {
   description = "the region where the vpc would be created"
-  type = string
-  default = "ca-central-1"
+  type        = string
+  default     = "ca-central-1"
 }
+
+variable "instance_name" {
+  description = "value of the bastion host ec2 instance name"
+  type        = string
+  default     = "bastion"
+}
+
+variable "instance_type" {
+  description = "the ec2 instance's type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "allowed_ssh_cidrs" {
+  type        = list(string)
+  description = "CIDR blocks allowed to SSH to the bastion. use _ip/32"
+  default     = ["0.0.0.0/0"]
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key here"
+}
+
